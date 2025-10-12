6f91187e4343e35a36685810629ee43041454b9a
Mike Kittridge <mgkittridge@gmail.com>
2025-10-12 18:51:28 +1300

    updated

--- params.py
+++ params.py
@@ -49,35 +49,6 @@ wrf_nml_path = pathlib.Path('/namelist.input')
 ##########################################
 ### ERA5
 
-# era5_file_names = [
-#     'e5.oper.an.pl/{date}/e5.oper.an.pl.128_129_z.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.pl/{date}/e5.oper.an.pl.128_133_q.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.pl/{date}/e5.oper.an.pl.128_130_t.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.pl/{date}/e5.oper.an.pl.128_131_u.ll025uv.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.pl/{date}/e5.oper.an.pl.128_132_v.ll025uv.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_034_sstk.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_235_skt.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_039_swvl1.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_040_swvl2.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_041_swvl3.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_042_swvl4.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_139_stl1.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_170_stl2.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_183_stl3.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_236_stl4.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_031_ci.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_167_2t.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_168_2d.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_165_10u.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_166_10v.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_033_rsn.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_141_sd.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_151_msl.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.an.sfc/{date}/e5.oper.an.sfc.128_134_sp.ll025sc.{start_date_hour}_{end_date_hour}.nc',
-#     'e5.oper.invariant/197901/e5.oper.invariant.128_172_lsm.ll025sc.1979010100_1979010100.nc',
-#     'e5.oper.invariant/197901/e5.oper.invariant.128_172_z.ll025sc.1979010100_1979010100.nc',
-#     ]
-
 
 #######################################################
 ### Functions
@@ -92,6 +63,7 @@ def create_rclone_config(name, config_path, config_dict):
     config_str = ' '.join(config_list)
     config_path = config_path.joinpath('rclone.config')
     cmd_str = f'rclone config create {name} {type_} {config_str} --config={config_path} --non-interactive'
+    # print(cmd_str)
     cmd_list = shlex.split(cmd_str)
     p = subprocess.run(cmd_list, capture_output=True, text=True, check=True)
 
